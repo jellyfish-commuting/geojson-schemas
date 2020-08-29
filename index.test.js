1acda12d8de72c0782b3f967bb24b0a2e777306a
thejellyfish <the@jellyfish-commuting.com>
2020-08-29 23:51:44 +0200

    Jest tests

---
+++ index.test.js
@@ -0,0 +1,73 @@
+const Ajv = require('ajv');
+const Schemas = require('./index');
+
+// Start tests ...
+describe('GeoJSON validation', () => {
+  it('expect validate GeoJSON FeaturePoint', () => {
+    // Init validator
+    const ajv = new Ajv();
+    const validate = ajv.compile(Schemas.FeaturePoint);
+
+    expect(validate({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [102.0, 0.5]
+      },
+      properties: {
+        prop0: 'value0'
+      },
+    })).toBe(true);
+  });
+
+  it('expect unvalidate a bad GeoJSON FeaturePoint', () => {
+    // Init validator
+    const ajv = new Ajv();
+    const validate = ajv.compile(Schemas.FeaturePoint);
+
+    expect(validate({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [102.0, 0.5]
+      },
+      properties: true,
+    })).toBe(false);
+  });
+
+  it('expect validate GeoJSON FeatureLineString', () => {
+    // Init validator
+    const ajv = new Ajv();
+    const validate = ajv.compile(Schemas.FeatureLineString);
+
+    expect(validate({
+      type: 'Feature',
+      geometry: {
+        type: 'LineString',
+        coordinates: [[102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]],
+      },
+      properties: {
+        prop0: 'value0',
+        prop1: 0.0
+      }
+    })).toBe(true);
+  });
+
+  it('expect unvalidate a bad GeoJSON FeatureLineString', () => {
+    // Init validator
+    const ajv = new Ajv();
+    const validate = ajv.compile(Schemas.FeatureLineString);
+
+    expect(validate({
+      type: 'Feature',
+      geometry: {
+        type: 'LineString',
+        coordinates: [102.0, 0.5]
+      },
+      properties: {
+        prop0: 'value0',
+        prop1: 0.0
+      }
+    })).toBe(false);
+  });
+});
